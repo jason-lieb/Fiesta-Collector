132fd2286250502f00a7fb22ac57397255fafdb7
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-09 15:18:38 -0500

    created routes and api dirs index.js files

---
+++ routes/api/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const categoryRoutes = require('./categoryRoutes');
+const colorRoutes = require('./colorRoutes');
+const inventoryRoutes = require('./inventoryRoutes');
+const itemRoutes = require('./itemRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/category', categoryRoutes);
+router.use('/color', colorRoutes);
+router.use('/inventory', inventoryRoutes);
+router.use('/item', itemRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;

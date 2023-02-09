22469e114a8aea1e03d323100d7c689e9f33600c
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-09 14:16:03 -0500

    added index.html inside public folder and linked to server

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     //res.json({msg: "Pikachu is great!"});
-    res.send("<p> Sending an Object </p>");
-    //res.sendFile('index.html');
+    //res.send("<p> Sending an Object </p>");
+    res.sendFile('index.html');
 })
 
 module.exports = router;

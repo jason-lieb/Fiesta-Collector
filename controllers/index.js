e339ed47101f61728191cfe0d26a249b7a63c790
MasakiDesktop <mandij50@gmail.com>
2023-02-10 11:32:09 -0500

    changed the tailwind routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     //res.json({msg: "Pikachu is great!"});
     //res.send("<p> Sending an Object </p>");
-    res.sendFile('index.html');
+    res.sendFile('test.html');
 })
 
 module.exports = router;

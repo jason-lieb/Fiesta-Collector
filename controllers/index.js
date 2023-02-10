a3c764ec65678ea875655e99243f6491198ccd6e
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-10 11:22:12 -0500

    created handlebars infrastructure for front end development

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     //res.json({msg: "Pikachu is great!"});
     //res.send("<p> Sending an Object </p>");
-    res.sendFile('index.html');
+    //res.sendFile('index.html');
+    res.render('login');
 })
 
 module.exports = router;

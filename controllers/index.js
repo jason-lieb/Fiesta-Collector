33630bc6f6bcea057874c935a99992cf58238af2
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-10 15:24:03 -0500

    routing for post request complete

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const { User } = require('../models');
+// const router = require('express').Router();
+// const { User } = require('../models');
 
-router.get('/', (req, res) => {
-  //res.json({msg: "Pikachu is great!"});
-  //res.send("<p> Sending an Object </p>");
-  //res.sendFile('index.html');
-  res.render('login');
-});
+// router.get('/', (req, res) => {
+//   //res.json({msg: "Pikachu is great!"});
+//   //res.send("<p> Sending an Object </p>");
+//   //res.sendFile('index.html');
+//   res.render('login');
+// });
 
-module.exports = router;
+// module.exports = router;

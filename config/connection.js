162e00247e3a6abd2be2aba629f214e38afb98bf
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-09 20:53:03 -0500

    connected server to db logged 1+1 AS result

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+//let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
+//if (process.env.JAWSDB_URL) {
+  //sequelize = new Sequelize(process.env.JAWSDB_URL);
+//} else {
+  
+let sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
-      port: 3306
+      port: 3306,
     }
   );
-}
+//}
 
 module.exports = sequelize;

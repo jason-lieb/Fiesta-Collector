a58237732d577fc8261f943171a16b42310cebd4
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-10 14:50:20 -0500

    first front end to back end route

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
+const {User} = require('../models');
 
 router.get('/', (req, res) => {
     //res.json({msg: "Pikachu is great!"});
     //res.send("<p> Sending an Object </p>");
     //res.sendFile('index.html');
     res.render('login');
+})
+
+router.post('/login', async(req,res) => {
+    try{
+        const userInfo = await User.create({
+            name: req.body.nameFromFrontEnd,
+            email: req.body.emailFromFrontEnd,
+            password: req.body.passwordFromFrontEnd
+        })
+        res.status(200).json(userInfo);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,22 @@
-let inputEl = document.querySelector('#input');
 let buttonEl = document.querySelector('#button');
 
-console.log(inputEl);
 console.log(buttonEl);
+
+buttonEl.addEventListener('click', async (event) =>{
+    event.preventDefault();
+    let nameInputEl = document.querySelector('#nameInput').value.trim();
+    console.log(nameInputEl);
+    let emailInputEl = document.querySelector('#emailInput').value.trim();
+    let passwordInputEl = document.querySelector('#passwordInput').value.trim();
+    // console.log(inputEl);
+
+    const login = await fetch('/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            nameFromFrontEnd : nameInputEl,
+            emailFromFrontEnd : emailInputEl,
+            passwordFromFrontEnd :passwordInputEl
+        }),
+        headers: {"Content-Type": "application/json"},
+    });
+})

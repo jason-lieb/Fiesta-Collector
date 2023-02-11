96df3df451fad5712e5161980a5988e39021f70d
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-11 14:09:26 -0500

    added controller login route, added userRoutes logout route/session termination with two new session dependencies

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const session = require('express-session');
 const routes = require('./routes');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 5500;
+
+// Sets up session and connect to our Sequelize db
+const sess = {
+  secret: 'Super secret secret',
+  // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
+  cookie: {
+    // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
+    maxAge: 60 * 60 * 1000,
+    // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+    httpOnly: true,
+    // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+    secure: false,
+    // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  // Sets up session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -20,6 +46,8 @@ app.use(routes);
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    console.log('Running on port ' + PORT);
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    );
   });
 });

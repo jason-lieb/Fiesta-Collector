c39904afc483863ee33133fefabde35d28fddc42
MasakiDesktop <mandij50@gmail.com>
2023-02-10 21:00:45 -0500

    finished login without bg img

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       // backgroundImage: {
       //   'boho': "url('./img/orangeboho.png')"
       // }
+      fontFamily: {
+        'dosis': ['Dosis', 'serif']
+      }
     },
   },
   plugins: [],

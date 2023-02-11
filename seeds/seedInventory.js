b5edd6ff0563df0a52e71dc688a2bf315c2c7b16
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-11 18:45:45 -0500

    homecontroller where syntax/logic created

--- seeds/seedInventory.js
+++ seeds/seedInventory.js
@@ -7,6 +7,13 @@ const inventoryData = [
   { user_id: 1, item_id: 59, color_id: 25, quantity: 1 },
   { user_id: 1, item_id: 60, color_id: 11, quantity: 5 },
   { user_id: 1, item_id: 67, color_id: 13, quantity: 1 },
+
+  { user_id: 2, item_id: 6, color_id: 22, quantity: 67 },
+  { user_id: 2, item_id: 14, color_id: 37, quantity: 67 },
+  { user_id: 2, item_id: 15, color_id: 26, quantity: 67 },
+  { user_id: 2, item_id: 59, color_id: 25, quantity: 67 },
+  { user_id: 2, item_id: 60, color_id: 11, quantity: 67 },
+  { user_id: 2, item_id: 67, color_id: 13, quantity: 67 },
 ];
 
 const seedInventory = () => Inventory.bulkCreate(inventoryData);

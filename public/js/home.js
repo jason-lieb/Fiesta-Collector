8c26ba356c44634e986bd4127f5dfac660b726bf
Jason Lieb <Jason.lieb@outlook.com>
2023-02-12 13:35:29 -0500

    initialize init function in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,24 @@
-const logout = async () => {
-    const response = await fetch('/api/user/logout', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-    });
+const global = {};
 
-    if (response.ok) {
-        document.location.replace('/login');
-    } else {
-        alert ('Failed to log out');
-    }
+const logout = async () => {
+  const response = await fetch('/api/user/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/login');
+  } else {
+    alert('Failed to log out');
+  }
 };
 
+const init = () => {
+  const categories = document.getElementById('categoryFilters');
+  const colors = document.querySelector('#colorFilters');
+  console.log(categories);
+  console.log(colors);
+};
+
+init();
 document.querySelector('#logOutButton').addEventListener('click', logout);
